460b0e897b68e65010385506154cb3bc92970ba7
Jason Salinas <jasonsalinas2@my.whccd.edu>
2025-04-28 18:25:31 -0700

    Add project files.

---
+++ COMP003B.LectureActivity6/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace COMP003B.LectureActivity6.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string Name { get; set;}
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set;}
+
+        // Collection navigation property
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
