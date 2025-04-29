0573a2714880b86550e033f3ef8205489954cc52
Jason Salinas <jasonsalinas2@my.whccd.edu>
2025-04-28 18:40:49 -0700

    Added step 3 and step 4

--- COMP003B.LectureActivity6/Models/Course.cs
+++ COMP003B.LectureActivity6/Models/Course.cs
@@ -2,5 +2,13 @@
 {
     public class Course
     {
+        public int CourseId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        // Collection navigation property
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
